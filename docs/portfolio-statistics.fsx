25fbb8d0ff0dd0d7c1de210bda0ab2358d9dfc47
Nicholas Hirschey <nhirschey@users.noreply.github.com>
2022-02-06 22:13:44 +0000

    more weights

--- docs/portfolio-statistics.fsx
+++ docs/portfolio-statistics.fsx
@@ -33,7 +33,7 @@ $$w_i=(\text{positionValue}_i)/(\text{portfolioValue}_p)$$.
 
 ### Long positions
 
-Let's calculate weights for an example portfolio "A" consisting of
+Let's calculate weights for an example *Portfolio A* consisting of
 - \$100 invested in AAPL
 - \$300 invested in GOOG
 - \$500 invested in TSLA
@@ -41,9 +41,9 @@ Let's calculate weights for an example portfolio "A" consisting of
 These are all long positions, meaning that they have positive costs. 
 *)
 
-let aaplPosition = 100
-let googPosition = 300
-let tslaPosition = 500
+let aaplPosition = 100.0
+let googPosition = 300.0
+let tslaPosition = 500.0
 
 // This implies:
 
@@ -130,66 +130,91 @@ The investor's cash and stock balances at the end of each period will look somet
 | 1   | \$200        | -4      | \$25.       | -\$100               |\$100.   |
 | 2   | \$92         | 0       | \$27.       | \$0.                 |\$92     |
 
-Imagine that an investor holding PortfolioA decides that 
-they want to short \$250 worth of Meta.
+Let's now create a new portfolio, *Portfolio B*, that includes short sales and calculate weights. Assume that you start with *Portfolio A* and short \$150 of AMZN stock. This generates \$150 of cash that you have to put somewhere. For individual investors, often your broker puts it in bonds and gives you none of the interest. Institutional investors can get some of the interest or even reinvest the proceeds in something else. We will assume that we are an institution and can reinvest all of the short proceeds. We will take the \$150 and add \$50 to each of our AAPL, GOOG, and TLSA positions. 
+
 *)
+
+let amznPositionB = -150.0
+let aaplPositionB = aaplPosition + 50.0
+let googPositionB = googPosition + 50.0
+let tslaPositionB = tslaPosition + 50.0
+
+let portfolioValueB = 
+    amznPositionB +
+    aaplPositionB +
+    googPositionB +
+    tslaPositionB
+(***include-fsi-output***)
+
+(* Compare to *Portfolio A* *)
+portfolioValueA = portfolioValueB 
+(***include-fsi-output***)
 
 (**
- 
-- A long portfolio has portfolio weights that sum to 1.0 (or 100%). 
-- A zero-cost portfolio has portfolio weights that sum to 0.0.
-
+The weights in *Portfolio B*:
 *)
 
-type Position = { Id: string; Position: int; Price : decimal }
+let amznWeightB = amznPositionB / portfolioValueB
+(***include-fsi-output***)
+
+let aaplWeightB = aaplPositionB / portfolioValueB
+(***include-fsi-output***)
+
+let googWeightB = googPositionB / portfolioValueB
+(***include-fsi-output***)
+
+let tslaWeightB = tslaPositionB / portfolioValueB
+(***include-fsi-output***)
+
+(**
+The weights of *Portfolio B* also add up to one.
+*)
+
+amznWeightB + aaplWeightB + googWeightB + tslaWeightB
+(***include-fsi-output***)
+
+(**
+### Zero-cost portfolios
+
+Another type of portfolio that you will see is zero-cost portfolios.
+They are called self funding because the short sale proceeds
+fund the long investments. The portfolio weights add up to 0.
+You can scale weights relative to what they would be per \$ long or short.
+
+An example:
+*)
+
+// Portfolio C
+let koPosition = -50.0
+let hogPosition = 40.0
+let yumPosition = 10.0
+
+let scale = 50.0
+let koWeight = koPosition / scale
+let hogWeight = hogPosition / scale
+let yumWeight = yumPosition / scale
+
+koWeight + hogWeight + yumWeight
+(***include-fsi-weight***)
+
+
+(**
+Now show how to do stuff with a collection
+*)
+
+type Position = { Id: string; PositionValue: float }
 
 let portfolio =
-    [| { Id = "AAPL"; Position = 100; Price = 22.20m }
-       { Id = "AMZN"; Position = 20; Price = 40.75m }
-       { Id = "TSLA"; Position = 50; Price = 30.6m } |]
+    [ { Id = "AMZN"; PositionValue = amznPositionB }
+      { Id = "AAPL"; PositionValue = aaplPositionB }
+      { Id = "GOOG"; PositionValue = googPositionB }
+      { Id = "TSLA"; PositionValue = tslaPositionB } ]
 
-// We can do it on the fly
-portfolio
-|> Array.map(fun pos -> // getting position value
-       pos.Id,
-       (float pos.Position)*(float pos.Price))
-|> fun ps -> // calculating weights
-    let portfolioValue = 
-        ps 
-        |> Array.sumBy(fun (id, value) -> value)
-    ps
-    |> Array.map(fun (id, value) -> id, value / portfolioValue)
-
-
-// We can also define a bit more structure to get the same thing.
-// Proper functions with defined types are good for reusable code.
-type PositionValue = { Id : string; Value : float }
-type PositionWeight = { Id : string; Weight : float }
-
-let calcValue (x:Position) = 
-    { Id = x.Id
-      Value = (float x.Position) * (float x.Price)}
-
-let calcWeights xs =
-    let portfolioValue = xs |> Array.sumBy(fun x -> x.Value)
-    xs
-    |> Array.map(fun pos -> 
-        { Id = pos.Id
-          Weight = pos.Value / portfolioValue })
-
-portfolio
-|> Array.map calcValue
-|> calcWeights
-
-let portfolioWithShorts =
-    [| { Id = "AAPL"; Position = 100; Price = 22.20m }
-       { Id = "AMZN"; Position = -20; Price = 40.75m }
-       { Id = "TSLA"; Position = 50; Price = 30.6m } |]
-
-portfolioWithShorts
-|> Array.map calcValue
-|> calcWeights
-
+let positionValues = [ for p in portfolio -> p.PositionValue ]
+let portfolioValue = positionValues |> List.sum
+let portfolioWeights =
+    [ for p in portfolio -> 
+        p.Id, p.PositionValue / portfolioValue ]
 
 (**
 ## Mean and Variance of Portfolio Returns

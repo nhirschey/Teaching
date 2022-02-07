aae50da102e60e6ad3e91257a1aa01b1d1c32f31
Nicholas Hirschey <nhirschey@users.noreply.github.com>
2022-02-07 14:53:25 +0000

    wrap up weights

--- docs/portfolio-statistics.fsx
+++ docs/portfolio-statistics.fsx
@@ -33,7 +33,122 @@ $$w_i=(\text{positionValue}_i)/(\text{portfolioValue}_p)$$.
 
 ### Long positions
 
+When an investor buys a long position, 
+they pay for the position now and hope to sell it later at a higher price.
+
+Let's look at cash flows for long positions.
+
+- At time 0: investor has \$100 cash and no shares.
+- At time 1: investor takes their \$100 of cash and buys 4 shares 
+  of stock at a price of \$25 per share. They have no cash and are long 4 shares.
+- At time 2: the stock has risen to \$27, the investor sells their 4 shares.
+  They have \$108 of cash and no shares.
+
+We can define some functions to update an account given these trades.
+*)
+
+// A record type that defines an account
+type AccountBalances =
+    { Time: int
+      Cash: float 
+      Shares: float }
+
+// A record type that defines a trade
+type Trade = 
+    { Shares: float 
+      Price : float }
+
+let accountAt0 = { Time = 0; Cash = 100.0; Shares = 0.0 }
+let tradeAt1 = { Shares = 4.0; Price = 25.0 }
+
+// `updateAccount` is a function that updates an account after a trade is made.
+// 
+// (trade: Trade) restricts the `trade` parameter to data of type `Trade`.
+//
+// (inAccount: AccountBalances) restricts the `inAccount` parameter 
+// to data of type `AccountBalances`
+//
+let updateAccount (trade: Trade) (inAccount: AccountBalances) =
+    let tradeValue = trade.Price * trade.Shares
+    let newCash = inAccount.Cash - tradeValue
+    let newShares = inAccount.Shares + trade.Shares
+    let newTime = inAccount.Time + 1
+    { Time = newTime 
+      Cash = newCash 
+      Shares = newShares }
+
+(** 
+You can make names with spaces using `` before and after.
+*)
+let ``expected account at t1`` = { Time = 1; Cash = 0.0; Shares = 4.0}
+let ``actual account at t1`` = updateAccount tradeAt1 accountAt0 
+(***include-fsi-output***)
+
+
+if ``actual account at t1`` <> ``expected account at t1`` then
+    failwith "You are not updating account correctly after a trade"
+
+(**
+Now we can calculate how the account value changes over time.
+*)
+
+let accountAt1 = updateAccount tradeAt1 accountAt0
+(***include-fsi-output***)
+
+let tradeAt2 = { Shares = -4.0; Price = 27.0 }
+let accountAt2 = updateAccount tradeAt2 accountAt1
+(***include-fsi-output***)
+
+(**
+We could have also written this code using the pipe operator.
+*)
+
+let accountAt1' = accountAt0 |> updateAccount tradeAt1 // same as "updateAccount tradeAt1 accountAt0"
+let accountAt2' = accountAt1 |> updateAccount tradeAt2 // same as "updateAccount tradeAt2 accountAt1"
+
+(**
+The pipe operator does not look very useful above because 
+we are only doing one operation.
+It is more useful when you're doing a series of multiple operations.
+This example recomputes the account value at time 2 by
+chaining together updates for the trades at times 1 and 2.
+
+*)
+
+let accountAt2'' =
+    accountAt0
+    |> updateAccount tradeAt1
+    |> updateAccount tradeAt2
+
+(***include-fsi-output***)
+
+(**
+This code is closer to how you would describe it in English:
+"Start with the account at time 0, 
+update it for the trade at time 1,
+then update it for the trade at time 2."
+*)
+
+(**
+> Practice: complete the code for the `accountValue` function below.
+It should calculate total account value of
+the stock and cash positiions. 
+If it is correct then the account value test below should evaluate to `true`
+*)
+
+let accountValue (stockPrice: float) (account: AccountBalances) =
+    failwith "unimplemented"
+
+(***do-not-eval***)
+// simple account value test
+(accountValue 27.0 accountAt2) = 108.0
+
+(**
+### Portfolio weights of long positions
+
+Now that we understand long positions we can calculate portfolio weights for them.
 Let's calculate weights for an example *Portfolio A* consisting of
+
 - \$100 invested in AAPL
 - \$300 invested in GOOG
 - \$500 invested in TSLA
@@ -41,13 +156,13 @@ Let's calculate weights for an example *Portfolio A* consisting of
 These are all long positions, meaning that they have positive costs. 
 *)
 
-let aaplPosition = 100.0
-let googPosition = 300.0
-let tslaPosition = 500.0
+let aaplPositionValue = 100.0
+let googPositionValue = 300.0
+let tslaPositionValue = 500.0
 
 // This implies:
 
-let portfolioValueA = aaplPosition + googPosition + tslaPosition
+let portfolioValueA = aaplPositionValue + googPositionValue + tslaPositionValue
 
 (***include-fsi-output***)
 
@@ -55,13 +170,13 @@ let portfolioValueA = aaplPosition + googPosition + tslaPosition
 The portfolio weights are then
 *)
 
-let aaplWeight = aaplPosition / portfolioValueA
+let aaplWeight = aaplPositionValue / portfolioValueA
 (***include-fsi-output***)
 
-let googWeight = googPosition / portfolioValueA
+let googWeight = googPositionValue / portfolioValueA
 (***include-fsi-output***)
 
-let tslaWeight = tslaPosition / portfolioValueA
+let tslaWeight = tslaPositionValue / portfolioValueA
 (***include-fsi-output***)
 
 (**
@@ -94,26 +209,8 @@ When an investor buys a long position,
 they pay for the position now and hope to sell it later at a higher price.
 A short sale reverses this.
 The investor sells the position now and hopes to buy it back later at a lower price.
-To see how this works, it is helpful to compare the cash flows
-from a long purchase to those from a short sale.
 
-First, a long purchase.
-
-- At time 0: investor has \$100 cash and no shares.
-- At time 1: investor takes their \$100 of cash and buys 4 shares 
-  of stock at a price of \$25 per share. They have no cash and are long 4 shares.
-- At time 2: the stock has risen to \$27, the investor sells their 4 shares.
-  They have \$108 of cash and no shares.
-
-The investor's cash and stock balances at the end of each period will look something like 
-
-| Time | Cash Balance | Shares | Share Price | Stock Position Value | Account Value |
---------------------------------------------------------------------------------------
-| 0   | \$100.       | 0       | \$25.       | \$0                  |\$100.   |
-| 1   | \$0.         | 4       | \$25.       | \$100                |\$100.   |
-| 2   | \$108.       | 0       | \$27.       | \$0.                 |\$108.   |
-
-Now, a short position.
+We now go through an example to see how the cash flows work.
 
 - At time 0: investor has \$100 cash and no shares.
 - At time 1: investor borrows 4 shares of stock and sells them
@@ -124,29 +221,45 @@ Now, a short position.
 
 The investor's cash and stock balances at the end of each period will look something like 
 
-| Time | Cash Balance | Shares | Share Price | Stock Position Value | Account Value |
---------------------------------------------------------------------------------------
-| 0   | \$100.       | 0       | \$25.       | \$0                  |\$100.   |
-| 1   | \$200        | -4      | \$25.       | -\$100               |\$100.   |
-| 2   | \$92         | 0       | \$27.       | \$0.                 |\$92     |
-
-Let's now create a new portfolio, *Portfolio B*, that includes short sales and calculate weights. Assume that you start with *Portfolio A* and short \$150 of AMZN stock. This generates \$150 of cash that you have to put somewhere. For individual investors, often your broker puts it in bonds and gives you none of the interest. Institutional investors can get some of the interest or even reinvest the proceeds in something else. We will assume that we are an institution and can reinvest all of the short proceeds. We will take the \$150 and add \$50 to each of our AAPL, GOOG, and TLSA positions. 
-
 *)
 
-let amznPositionB = -150.0
-let aaplPositionB = aaplPosition + 50.0
-let googPositionB = googPosition + 50.0
-let tslaPositionB = tslaPosition + 50.0
-
-let portfolioValueB = 
-    amznPositionB +
-    aaplPositionB +
-    googPositionB +
-    tslaPositionB
+let shortAt1 = { Shares = -4.0; Price = 25.0 }
+let shortCoverAt2 = { Shares = 4.0; Price = 27.0 }
 (***include-fsi-output***)
 
-(* Compare to *Portfolio A* *)
+// positions at t1
+accountAt0 
+|> updateAccount shortAt1
+(***include-fsi-output***)
+
+// positions at t2
+accountAt0 
+|> updateAccount shortAt1 
+|> updateAccount shortCoverAt2
+(***include-fsi-output***)
+
+(**
+
+### Portfolio weights for short positions
+
+Let's create a new portfolio, *Portfolio B*, that includes short sales and calculate weights. Assume that you start with *Portfolio A* and short \$150 of AMZN stock. This generates \$150 of cash that you have to put somewhere. For individual investors, often your broker puts it in bonds and gives you none of the interest. Institutional investors can get some of the interest or even reinvest the proceeds in something else. We will assume that we are an institution and can reinvest all of the short proceeds. We will take the \$150 and add \$50 to each of our AAPL, GOOG, and TLSA positions. 
+
+Short positions have negative portfolio weights.
+*)
+
+let amznPositionValueB = -150.0
+let aaplPositionValueB = aaplPositionValue + 50.0
+let googPositionValueB = googPositionValue + 50.0
+let tslaPositionValueB = tslaPositionValue + 50.0
+
+let portfolioValueB = 
+    amznPositionValueB +
+    aaplPositionValueB +
+    googPositionValueB +
+    tslaPositionValueB
+(***include-fsi-output***)
+
+(** Compare to *Portfolio A* *)
 portfolioValueA = portfolioValueB 
 (***include-fsi-output***)
 
@@ -154,16 +267,16 @@ portfolioValueA = portfolioValueB
 The weights in *Portfolio B*:
 *)
 
-let amznWeightB = amznPositionB / portfolioValueB
+let amznWeightB = amznPositionValueB / portfolioValueB
 (***include-fsi-output***)
 
-let aaplWeightB = aaplPositionB / portfolioValueB
+let aaplWeightB = aaplPositionValueB / portfolioValueB
 (***include-fsi-output***)
 
-let googWeightB = googPositionB / portfolioValueB
+let googWeightB = googPositionValueB / portfolioValueB
 (***include-fsi-output***)
 
-let tslaWeightB = tslaPositionB / portfolioValueB
+let tslaWeightB = tslaPositionValueB / portfolioValueB
 (***include-fsi-output***)
 
 (**
@@ -185,42 +298,108 @@ An example:
 *)
 
 // Portfolio C
-let koPosition = -50.0
-let hogPosition = 40.0
-let yumPosition = 10.0
+let koPositionValue = -50.0
+let hogPositionValue = 40.0
+let yumPositionValue = 10.0
 
-let scale = 50.0
-let koWeight = koPosition / scale
-let hogWeight = hogPosition / scale
-let yumWeight = yumPosition / scale
+let dollarsLong = 50.0
+let koWeight = koPositionValue / dollarsLong
+let hogWeight = hogPositionValue / dollarsLong
+let yumWeight = yumPositionValue / dollarsLong
 
 koWeight + hogWeight + yumWeight
-(***include-fsi-weight***)
+(***include-fsi-output***)
 
 
 (**
-Now show how to do stuff with a collection
+### Calculating weights using a list of positions
+
+The calculations that we did thus far required code for each position.
+We did the same thing to each position, so there was some repetition.
+We can reduce the repetition by putting the positions into a list
+and then operating on the elements of the list via iteration.
 *)
 
+// defining a position record type
 type Position = { Id: string; PositionValue: float }
 
+// assigning a list of positions to a value named portfolio
 let portfolio =
-    [ { Id = "AMZN"; PositionValue = amznPositionB }
-      { Id = "AAPL"; PositionValue = aaplPositionB }
-      { Id = "GOOG"; PositionValue = googPositionB }
-      { Id = "TSLA"; PositionValue = tslaPositionB } ]
+    [ { Id = "AMZN"; PositionValue = amznPositionValueB }
+      { Id = "AAPL"; PositionValue = aaplPositionValueB }
+      { Id = "GOOG"; PositionValue = googPositionValueB }
+      { Id = "TSLA"; PositionValue = tslaPositionValueB } ]
 
+// This is called a list comprehension
 let positionValues = [ for p in portfolio -> p.PositionValue ]
+(***include-value:positionValues***)
+
+(**
+The list module has many different functions for operating on lists. 
+If you type `List.` you should see many different functions pop up.
+These functions are very useful. We will explore them in more detail later.
+
+For now, let's see what `List.map` does.
+*)
+
+portfolio |> List.map (fun p -> p.PositionValue)
+(***include-fsi-output***)
+
+(**
+This is the same result as the `positionValues` value that we calculated
+using the list comprehension. 
+`List.map` "maps" each element of the list to an output using a function.
+In this case, our function `(fun p -> p.PositionValue)` was an anonymous function.
+
+Another useful function from the list module is `List.sum`.
+We can use it to calculate the total value of the portfolio by
+summing position values.
+*)
+
 let portfolioValue = positionValues |> List.sum
+(***include-value: portfolioValue***)
+
+(**
+And with this we can calculate portfolio weights.
+*)
 let portfolioWeights =
     [ for p in portfolio -> 
-        p.Id, p.PositionValue / portfolioValue ]
+        let weight = p.PositionValue / portfolioValue 
+        p.Id, weight ]
+portfolioWeights
+(***include-value: portfolioWeights***)
 
 (**
 ## Mean and Variance of Portfolio Returns
+
+### A portfolio's return.
+
+A portfolio's return is the weighted average return of the portfolio's positions.
+
+$$ r_p = \Sigma^N_{i=1} w_i r_i,$$
+where $r$ is return, $i$ indexes stocks, and $w$ is portfolio weights.
 *)
 
+type PositionsWithReturn =
+    { Id: string 
+      Weight: float 
+      Return: float }
+
+let exPortfolio =
+    [ { Id = "A"; Weight = 0.25; Return = 0.1 }
+      { Id = "B"; Weight = 0.75; Return = 0.2 } ]
+
+let weightsXreturn = [ for pos in exPortfolio -> pos.Weight * pos.Return ]
+weightsXreturn
+(***include-value:weightsXreturn***)
+
+let exPortfolioReturn = weightsXreturn |> List.sum 
+exPortfolioReturn
+(***include-value: exPortfolioReturn***)
+
 (**
+
+For next time: Portfolio Variance and Leverage
 ## Leverage
 *)
 

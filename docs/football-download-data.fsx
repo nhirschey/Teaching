8b204245e864d976a9155079b9a120f77720019f
Nicholas Hirschey <nhirschey@users.noreply.github.com>
2022-02-15 20:40:05 +0000

    simplify scraping

--- docs/football-download-data.fsx
+++ docs/football-download-data.fsx
@@ -7,12 +7,21 @@ index: 1
 ---
 *)
 
+(***hide***)
+// original link:
+// https://fbref.com/en/comps/Big5/2019-2020/stats/players/2019-2020-Big-5-European-Leagues-Stats
+
 (**
 # Webscraping HTML to CSV
-For the **Football Players' Exercises** we are trying to scrape the data from [Football References](https://fbref.com/en/comps/Big5/2019-2020/stats/players/2019-2020-Big-5-European-Leagues-Stats) website. This data consists on the stats of the players that played in the big 5 football european leagues in the 2019-2020 season. Since we need to scrape the data from a webpage and store it in a csv file we will need to use 2 type providers from [FSharp.Data](https://fsharp.github.io/FSharp.Data/):  
+
+> Originally developed by [Davide Costa](https://github.com/DavideGCosta)
+
+For the **Football Players' Exercises** we are trying to scrape data from [Sports Reference's](https://www.sports-reference.com/) [Football Reference](https://fbref.com/en/) website. The particular data that we want to scrap is player data from the [Big 5 European Leauges table](https://fbref.com/en/comps/Big5/stats/players/Big-5-European-Leagues-Stats). This data consists on the stats of the players that played in the big 5 football european leagues in the most recent season. Since we need to scrape the data from a webpage and store it in a csv file we will need to use 2 type providers from [FSharp.Data](https://fsharp.github.io/FSharp.Data/):
+
 - We'll use the [HTML Type Provider](https://fsprojects.github.io/FSharp.Data/library/HtmlProvider.html) to scrape the html from the webpage and access the players' table.  
-- We'll use the [CSV Type Provider](https://fsprojects.github.io/FSharp.Data/library/CsvProvider.html) to construct the csv file with the players' table.  
-Both type Providers are located in `FSharp.Data` assembly. So in order to use it we need to load and open `FSharp.Data`.
+- We'll use the [CSV Type Provider](https://fsprojects.github.io/FSharp.Data/library/CsvProvider.html) to save the data to a csv file.  
+
+Both type providers are located in `FSharp.Data` assembly. To use it we need to load and open `FSharp.Data`.
 *)
 
 #r "nuget: FSharp.Data"
@@ -25,60 +34,62 @@ We do this by providing a sample of the data.
 The type provider uses the sample to generate code to read data with that format. 
 *)
 
-type Big5EuropeanLeagues = HtmlProvider<"https://fbref.com/en/comps/Big5/2019-2020/stats/players/2019-2020-Big-5-European-Leagues-Stats">
+type Big5EuropeanLeagues = 
+    HtmlProvider<"https://fbref.com/en/comps/Big5/stats/players/Big-5-European-Leagues-Stats">
 
 (**
-The type `Big5EuropeanLeagues` contains information about that web page. It knows what the html is, it knows what the html tables are, etc.
+The type `Big5EuropeanLeagues` contains information about the structure of the web page. It knows what the html is, it knows what the html tables are, etc.
 *)
 
 (**
-Now that we have the `Big5EuropeanLeagues` HTML type we can either use `Load(url)` or `GetSample()` to get the html of the webpage we want to webscrape.  
-- `Big5EuropeanLeagues.GetSample()` gets the HTML of the sample url used to define the HTML type, this is why there's no need in specifiying the url as a parameter within brackets.    
-- `Big5EuropeanLeagues.Load(url)` gets the HTML of the url that corresponds to the same format as the one defined in the HTML Provider.  
+Now that we have the `Big5EuropeanLeagues` HTML type defined we can use `GetSample()` to load the sample webpage
+that we used to define the type.  
+
 *)
-let Big5EuropeanLeaguesHTML = Big5EuropeanLeagues.GetSample()
-// or --> In this case both work but if you are trying to scrape a different webpage with the same type, you should use Load.
-let Big5EuropeanLeaguesHTML1 = Big5EuropeanLeagues.Load("https://fbref.com/en/comps/Big5/2019-2020/stats/players/2019-2020-Big-5-European-Leagues-Stats")
+let big5 = Big5EuropeanLeagues.GetSample()
 
 (**
-Now, we have the HTML of the webpage stored in the variable `Big5EuropeanLeaguesHTML`. Let's observe the first 200 characters of the HTML.
+Now, we have the HTML of the webpage stored in the variable `big5`. Let's observe the first 200 characters of the HTML.
 *)
-Big5EuropeanLeaguesHTML.Html.ToString().[.. 200]
+(***do-not-eval***)
+big5.Html.ToString()[..200]
+(***hide***)
+big5.Html.ToString().[..200]
 (*** include-it ***)
 (**
-From the tables available in the webpage, let's assign the `Player Standard Stats 2019-2020 Big 5 European Leagues` to a variable called `footballPlayersTable`.
-*)
-let footballPlayersTable = Big5EuropeanLeaguesHTML.Tables.``Player Standard Stats 2019-2020 Big 5 European Leagues``
-
-(**
-Now let's observe the first 3 table rows by using the array property `Rows` and `Array.truncate`.
-*)
-footballPlayersTable.Rows
-|> Array.truncate 3
-(*** include-it ***)
-(**
-Let's look at the header' fields available in the table using the property `Headers`.
-*)
-footballPlayersTable.Headers
-(*** include-it ***)
-(**
-Using properties `Rows` and `Array.Map` we can also observe the table fields, for instance let´s observe the first 5 rows of fields `Player` and `Age`.
-*)
-footballPlayersTable.Rows
-|> Array.map(fun x -> x.Player)
-|> Array.take 5
-(*** include-it ***)
-(**
+From the tables available in the webpage, let's assign the `Player Standard Stats 2019-2020 Big 5 European Leagues` to a variable called `footballers`.
 *)
 
-footballPlayersTable.Rows
-|> Array.map(fun x -> x.Age)
-|> Array.take 5
+let footballers = big5.Tables.``Player Standard Stats 2019-2020 Big 5 European Leagues``
+
+(**
+Now let's observe the first 3 table rows. The `Rows` property gives us an array of rows.
+We can index into the array using `[..2]` to limit it to the first 3 rows.
+*)
+(***do-not-eval***)
+footballers.Rows[..2]
+(***hide***)
+footballers.Rows.[..2]
 (*** include-it ***)
+(**
+Let's look at the header fields in the table using the property `Headers`.
+*)
+footballers.Headers
+(*** include-it ***)
+(**
+Let´s look at the first 5 rows of fields `Player` and `Age`.
+*)
+(***do-not-eval***)
+[ for row in footballers.Rows[..4] do row.Player, row.Age]
+(***hide***)
+[ for row in footballers.Rows.[..4] do row.Player, row.Age]
+(*** include-it ***)
+
 (**
  The table data is not exactly what we need. To work with it, we need to clean it up.
+
 - **Repeated Headers.**
-    - If we run `playerStatsTable.Rows.[25]` we can see that it corresponds to the headers and not to a player row. This means that the header is in line 0 and then repeats in every row multiple of 25.  
+    - If we run `footballers.Rows[25]` we can see that it corresponds to the headers and not to a player row. This means that the header is in line 0 and then repeats after every 25 players.  
     - We remove those lines by using `Array.filter(fun x -> x.Player <> "Player")`. This code filters the data set to include only rows where the player's name is not equal to the string `"Player"`.  
 - **Age and Goals Scored with empty strings.**    
     - In order to make some arithmetic operations with age and goals scored, we need to convert the data into integers or floats/decimals.  
@@ -86,10 +97,12 @@ footballPlayersTable.Rows
     - We remove those lines by using `Array.filter(fun x -> x.Age <> "" and x.Performance - Gls <> "")`.  
 *)
 
-let footballPlayersTableParsed =
-    footballPlayersTable.Rows
-    |> Array.filter(fun x -> 
-        x.Rk <> "Rk" && 
+let footballersParsed =
+    footballers.Rows
+    // Array.filter 
+    |> Array.filter (fun x -> 
+        //
+        x.Player <> "Player" && 
         x.Age <> "" && 
         x.``Performance - Gls`` <> "")
     |> Array.sortByDescending(fun x -> 
@@ -105,8 +118,9 @@ We need to save the data to a Csv file in order to use it in the Exercises.
 
 (**
 First we need to construct a sample of the data that will be stored in the Csv File.
-By running `footballPlayersTable.Headers` as done previously, we can easily observe that the table has fields that will not be used.  
+By running `footballers.Headers` as done previously, we can easily observe that the table has fields that will not be used.  
 The csv file doesn't need to contain all those fields, we only need: 
+
 - `Player` (Players' Name)  
 - `Nation` (Players' Nationality)  
 - `Pos` (Players' Position)  
@@ -115,7 +129,9 @@ The csv file doesn't need to contain all those fields, we only need:
 - `Age` (Players' Age)   
 - `Playing Time - MP` (Players' Matches Played)  
 - `Performance - Gls` (Players' Goals Scored)  
+
 For the sample provider we provide the header and 2 example rows. The header gives the field names. The example rows give example data that lets the type provider figure out the type of the fields.
+
 - The only point of `player1` and `aardvark` is to point out that the player field contains `strings`. You could use any string in place of `player1` or `aardvark`. The same is true for the `Nation`, `Position`, `Team` and `League` fields.  
 - For `Age`, `MatchesPlayed` and `GoalsScored`, we use `00` to represent that the field contains `integers`.  You could use any other `integer` for the sample.  
 *)
@@ -137,7 +153,7 @@ Now that we have the data and the Csv sample let's create an "array of CSV rows"
 *)
 
 let FootballPlayersCsvRows = 
-    footballPlayersTableParsed
+    footballersParsed
     |> Array.map(fun htmlRow -> 
         FootballPlayersCsv.Row
             ( player = htmlRow.Player,
@@ -151,7 +167,7 @@ let FootballPlayersCsvRows =
 
 (**
 The steps are:
-1. We start with the array that contains the data (`footballPlayersTableParsed`).  
+1. We start with the array that contains the data (`footballersParsed`).  
 2. Then we use `Array.map` to iterate through the rows of the html table and convert each row of the html table into a `FootballPlayersCsv.Row`.  
     - We don't want all the fields from the html table. We only want the ones specified in the `FootballPlayersCsv` type.  
     - So for each field in a `FootballPlayersCsv` row we assign the corresponding field from the html table row.  
@@ -161,8 +177,7 @@ The steps are:
 (**
 Let's look at the first 5 rows of `FootballPlayersCsvRows` using the `Array.truncate` property. We don't need to use `FootballPlayersCsvRows.Rows` because the variable is already an array of csv rows.  
 *)
-FootballPlayersCsvRows
-|> Array.truncate 5
+FootballPlayersCsvRows[0..4]
 (*** include-it ***)
 (**
 Now we have the data we need, but

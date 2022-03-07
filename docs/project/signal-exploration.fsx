d8c4c90d6c8184f0be173e62f6ca8424044fde2c
Nicholas Hirschey <nhirschey@users.noreply.github.com>
2022-03-07 14:54:45 +0000

    midterm project stub.

---
+++ docs/project/signal-exploration.fsx
@@ -0,0 +1,52 @@
+(**
+---
+title: Signal Exploration
+category: Project
+categoryindex: 5
+index: 1
+---
+
+[![Binder](../img/badge-binder.svg)](https://mybinder.org/v2/gh/nhirschey/teaching/gh-pages?filepath={{fsdocs-source-basename}}.ipynb)&emsp;
+[![Script](../img/badge-script.svg)]({{root}}/{{fsdocs-source-basename}}.fsx)&emsp;
+[![Notebook](../img/badge-notebook.svg)]({{root}}/{{fsdocs-source-basename}}.ipynb)
+
+
+**Student Name:**
+
+**Student Number:**
+
+**Signal Name (e.g., Book to Market):**
+
+**Signal Code (e.g., be_me):**
+
+This is the mid-term project. It is an exercise in exploratory data analysis, using your assigned trading signal as the data. There is a written component called **Signal Background** and a data analysis component called **Signal Analysis**. 
+
+## Signal Background
+
+Provide a written description of the signal you are using. Write the text in this cell after deleting the text explaining what to write.
+
+Use a *maximum* of 1,000 words. The word count for a notbook cell is displayed at the bottom bar of the VS Code window when you are editing a cell. Notebook text cells are written in [Markdown](https://en.wikipedia.org/wiki/Markdown). You can find a short tutorial for markdown syntax at [https://commonmark.org/help/](https://commonmark.org/help/).
+
+- Describe your signal and explain how it is constructed.
+- Explain the economic motivation for why this may be a useful predictor of security returns or risk. Include your own perspective and any relevant outside perspectives (original study authors, famous investors, popular media, etc.).
+
+*)
+
+(**
+## Signal Analysis
+
+### First, make sure that you're referencing the correct files.
+
+Here I'm assuming that you have a class folder with this `signal-exploration.ipynb` notebook and a `data` folder inside of it. The folder hierarchy would look like below where you
+have the below files and folders accessible:
+
+```code
+/class
+    signal-exploration.ipynb
+    /data
+        id_and_return_data.csv
+        zero_trades_252d.csv
+    
+```
+
+*)
